c371e9e95f60e383239a8b8c22b09cfee18c3a24
Valéria Cabral Nunes <100439216+valerianunes@users.noreply.github.com>
2022-04-25 18:03:47 -0300

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,9 @@
+# 10. Faça um Programa que peça um número correspondente a um determinado
+# ano e em seguida informe se este ano é ou não bissexto.
+
+ano = int(input("Digite o ano: "))
+
+if((((ano % 4) == 0) and ((ano % 100) != 0))) or ((ano % 400) == 0):
+    print("Ano Bissesto")
+else:
+    print("Não é Bissesto")
